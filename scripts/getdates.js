07d19649a5866cd85d68e9fd6bf80e17a7117244
Jgohnert <goh22001@byui.edu>
2024-05-10 09:30:48 -0600

    Removed hamburger menu code.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -9,11 +9,3 @@ currentyear.innerHTML = new Date().toLocaleDateString("en-US", year);
 const lastmodif = document.querySelector("#lastmodified");
 let lastModified = new Date(document.lastModified);
 lastmodif.innerHTML = lastModified.toLocaleString();
-
-const menuButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-menuButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	menuButton.classList.toggle('open');
-});
