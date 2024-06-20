7f0112d0082da338efde332ef957426f4169e4cd
Jgohnert <goh22001@byui.edu>
2024-06-20 15:42:45 -0600

    image URL fixed in membership.JSON file

--- chamber/scripts/member-list.js
+++ chamber/scripts/member-list.js
@@ -1,5 +1,4 @@
-const baseURL = "https://jgohnert.github.io/wdd230/";
-const membersURL = "https://jgohnert.github.io/wdd230/data/members.json";
+const membersURL = "https://jgohnert.github.io/wdd230/chamber/data/members.json";
 
 const memberCards = document.querySelector("#cards");
 
@@ -7,28 +6,39 @@ async function getMembers() {
         const response = await fetch(membersURL);
         const data = await response.json();
         console.table(data.members);
-        // displayMembers(data.members);
+        displayMembers(data.members);
 }
 
-// const displayMembers = (prophets) => {
-//     prophets.forEach((prophet) => {
-//         let card = document.createElement("section");
-//         let fullName = document.createElement("h2");
-//         let birth = document.createElement("p");
-//         let portrait = document.createElement("img");
-//         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
-//         birth.textContent = `Date of Birth: ${prophet.birthdate}`;
-//         portrait.setAttribute("src", prophet.imageurl);
-//         portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement("section");
+        let companyName = document.createElement("h2");
+        let companyAddress = document.createElement("p");
+        let companyPhone = document.createElement("p");
+        let companySite = document.createElement("p");
+        let companyMembership = document.createElement("p");
+        let companyLogo = document.createElement("img");
+        companyName.textContent = `${member.name}`;
+        companyAddress.textContent = `Address: ${member.address}`;
+        companyPhone.textContent = `${member.phone}`;
+        companySite.textContent = `${member.URL}`;
+        companyMembership.textContent = `Membership Level: ${member.membership}`;
+        companyLogo.setAttribute("src", member.image);
+        companyLogo.setAttribute("alt", `Portrait of ${member.name}`);
 
-//         portrait.setAttribute("loading", "lazy");
-//         portrait.setAttribute("width", "340");
-//         portrait.setAttribute("height", "440");
+        companyLogo.setAttribute("loading", "lazy");
+        companyLogo.setAttribute("width", "350");
+        companyLogo.setAttribute("height", "120");
 
-//         card.appendChild(fullName);
-//         card.appendChild(birth);
-//         card.appendChild(portrait);
+        card.appendChild(companyName);
+        card.appendChild(companyAddress);
+        card.appendChild(companyPhone);
+        card.appendChild(companySite);
+        card.appendChild(companyMembership);
+        card.appendChild(companyLogo);
 
-//         cards.appendChild(card);
-//     });
-// }
+        memberCards.appendChild(card);
+    });
+}
+
+getMembers();
