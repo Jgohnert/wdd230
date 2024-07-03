1926005bcf2320f378a4ddd9b5e4ebdf637ead1f
Jgohnert <goh22001@byui.edu>
2024-07-03 16:21:04 -0500

    The homepage now displays three random silver and gold members underneath company spotlights.

--- chamber/scripts/spotlight.js
+++ chamber/scripts/spotlight.js
@@ -1,10 +1,46 @@
 const membersURL = "https://jgohnert.github.io/wdd230/chamber/data/members.json";
-
 const memberCards = document.querySelector(".spotlight-card");
 
 async function getMembers() {
-    const response = await fetch(membersURL);
-    const data = await response.json();
-    console.table(data.members);
-    displayMembers(data.members);
+    try {
+        const response = await fetch(membersURL);
+        const data = await response.json();
+        const spotlightMembers = data.members.filter(member => member.membership === "Silver" || member.membership === "Gold");
+        displayRandomMembers(spotlightMembers);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
+
+function getRandomMembers(members, num) {
+    const randomMembers = [];
+    while (randomMembers.length < num && members.length > 0) {
+        const randomIndex = Math.floor(Math.random() * members.length);
+        const randomMember = members.splice(randomIndex, 1)[0];
+        randomMembers.push(randomMember);
+    }
+    return randomMembers;
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement("section");
+        let companyLogo = document.createElement("img");
+
+        companyLogo.setAttribute("src", member.image);
+        companyLogo.setAttribute("alt", `Portrait of ${member.name}`);
+        companyLogo.setAttribute("loading", "lazy");
+        companyLogo.setAttribute("width", "350");
+        companyLogo.setAttribute("height", "120");
+
+        card.appendChild(companyLogo);
+        memberCards.appendChild(card);
+    });
+}
+
+function displayRandomMembers(members) {
+    const randomMembers = getRandomMembers([...members], 3);
+    displayMembers(randomMembers);
+}
+
+getMembers();
