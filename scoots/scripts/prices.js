ad917988d617db4063a060c24ed23c1184c496e5
Jgohnert <goh22001@byui.edu>
2024-07-15 21:34:04 -0500

    Prices and scooter information is finished.

--- scoots/scripts/prices.js
+++ scoots/scripts/prices.js
@@ -1,10 +1,7 @@
 const pricesURL = "https://jgohnert.github.io/wdd230/scoots/data/prices.json";
 
-const table = document.querySelector("#table")
-const rentalName = document.querySelector("#motor-name");
-const maxseating = document.querySelector("#seats");
-const reservationPrice = document.querySelector("#res");
-const walkInPrice = document.querySelector("#non-res");
+const tableBody = document.querySelector("#table-body")
+const rentalDiv = document.querySelector("#rentals")
 
 async function getPrices() {
     const response = await fetch(pricesURL);
@@ -16,9 +13,36 @@ async function getPrices() {
 function displayMotorInfo(rentalType) {
 
     rentalType.forEach(rental => {
+        let table = document.createElement("tr");
+        let rentalName = document.createElement("td");
+        let maxseating = document.createElement("td");
+        let reservationPrice = document.createElement("td");
+        let walkInPrice = document.createElement("td");
+
         rentalName.textContent = rental.name;
         maxseating.textContent = rental.maxPersons;
         reservationPrice.textContent = `Half Day: ${rental.reservation[0].halfDay} Full Day: ${rental.reservation[1].fullDay}`;
+        walkInPrice.textContent = `Half Day: ${rental.walkIn[0].halfDay} Full Day: ${rental.walkIn[1].fullDay}`;
+
+        table.appendChild(rentalName);
+        table.appendChild(maxseating);
+        table.appendChild(reservationPrice);
+        table.appendChild(walkInPrice);
+        
+        tableBody.appendChild(table);
+
+
+        let rentalImgCard = document.createElement("section");
+        let imgName = document.createElement("h2");
+        let rentalimg = document.createElement("div");
+
+        imgName.textContent = rental.name;
+        rentalimg.innerHTML = `<img src="${rental.image}" alt="${rental.name}" width="300" height="300" loading="lazy">`
+
+        rentalImgCard.appendChild(imgName)
+        rentalImgCard.appendChild(rentalimg)
+
+        rentalDiv.appendChild(rentalImgCard)
 
     });
 
